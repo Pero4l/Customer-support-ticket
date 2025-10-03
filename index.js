441095abe25c2684e2b36e1eaa531ae377678870
pero4l <lilpero03@gmail.com>
2025-10-03 13:31:40 +0100

    routed it and want to taste it now

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+const userAuth = require('./router/user.route')
+
+
 app.get('/',(req,res)=>{
     return res.status(200).json({
         "success": true,
         "message": "WELLCOME TO PTB CUSTOMER SUPPORT"
     })
 })
+
+app.use('/auth',userAuth)
 
 
 const PORT = 4001
